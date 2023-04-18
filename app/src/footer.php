04f12b3dcf84339a1c3ebf22dd76d720247a22d3
Aayush Rijal <arijal@aiims.com.au>
2023-04-18 11:06:02 +1000

    Sticky phone number added

--- app/src/footer.php
+++ app/src/footer.php
@@ -1,3 +1,20 @@
+<div class="sticky-menu">
+	<div class="container px-0">
+		<div class="row justify-content-between gx-0">
+			<div class="col-6">
+				<a href="#about-spicefest" class="btn btn-dark rounded-0 text-white">
+					<svg width="17" height="19" viewBox="0 0 17 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M3.68741 8.34042C5.01875 11.0525 7.16369 13.2663 9.78014 14.6558L11.8141 12.5475C12.0638 12.2888 12.4336 12.2025 12.7572 12.3175C13.7926 12.6721 14.9113 12.8638 16.0578 12.8638C16.5663 12.8638 16.9823 13.295 16.9823 13.8221V17.1667C16.9823 17.6938 16.5663 18.125 16.0578 18.125C7.37633 18.125 0.340576 10.8321 0.340576 1.83333C0.340576 1.30625 0.75662 0.875 1.26512 0.875H4.50101C5.00951 0.875 5.42555 1.30625 5.42555 1.83333C5.42555 3.03125 5.61046 4.18125 5.95254 5.25458C6.05424 5.59 5.98028 5.96375 5.72141 6.23208L3.68741 8.34042Z" fill="white" />
+					</svg>
+					<span class="ps-1"><?= $phone_number ?></span>
+				</a>
+			</div>
+			<div class="col-6">
+				<a href="#form" class="btn btn-primary rounded-0 text-white py-3">Enquire Now</a>
+			</div>
+		</div>
+	</div>
+</div>
 
 <a href="javascript:" id="return-to-top">
 	<div class="d-flex justify-content-center align-items-center h-100 w-100">
